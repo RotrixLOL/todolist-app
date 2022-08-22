1191f63f3b3a1bfab9b9be0ded071ef21ec3dcf0
RotrixxLOL <ncusnir43@gmail.com>
2022-08-22 13:18:33 +0200

    change mongo connection

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))
 
-mongoose.connect(process.env.MONGO_CONNECT + 'todolistDB')
+mongoose.connect(process.env.MONGO_CONNECT + "todolistDB")
 
 const itemsSchema = {
   name: String
